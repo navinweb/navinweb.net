5e6a00a7d6731a50ddaa20f6107469a7f9c0709a
navinweb <anatoliykirzo@gmail.com>
2017-02-06 15:41:00 +0300

    #1 init project

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp           = require('gulp'),
+		gutil          = require('gulp-util' ),
+		sass           = require('gulp-sass'),
+		concat         = require('gulp-concat'),
+		uglify         = require('gulp-uglify'),
+		cleanCSS       = require('gulp-clean-css'),
+		rename         = require('gulp-rename'),
+		del            = require('del'),
+		imagemin       = require('gulp-imagemin'),
+		cache          = require('gulp-cache'),
+		autoprefixer   = require('gulp-autoprefixer'),
+		bourbon        = require('node-bourbon'),
+		ftp            = require('vinyl-ftp'),
+		notify         = require("gulp-notify");
+
+gulp.task('scripts', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/js/common.js',
+		])
+	.pipe(concat('scripts.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('app/js'))
+});
+
+gulp.task('sass', function() {
+	return gulp.src('app/sass/**/*.sass')
+	.pipe(sass({
+		includePaths: bourbon.includePaths
+	}).on("error", notify.onError()))
+	.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleanCSS())
+	.pipe(gulp.dest('app/css'))
+});
+
+gulp.task('watch', ['sass', 'scripts'], function() {
+	gulp.watch('app/sass/**/*.sass', ['sass']);
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['scripts']);
+});
+
+gulp.task('imagemin', function() {
+	return gulp.src('app/img/**/*')
+	.pipe(cache(imagemin()))
+	.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('build', ['removedist', 'imagemin', 'sass', 'scripts'], function() {
+
+	var buildFiles = gulp.src([
+		'app/*.html',
+		'app/*.htaccess',
+		'app/*.php',
+		]).pipe(gulp.dest('dist'));
+
+	var buildCss = gulp.src([
+		'app/css/main.min.css',
+		]).pipe(gulp.dest('dist/css'));
+
+	var buildJs = gulp.src([
+		'app/js/scripts.min.js',
+		]).pipe(gulp.dest('dist/js'));
+
+	var buildFonts = gulp.src([
+		'app/fonts/**/*',
+		]).pipe(gulp.dest('dist/fonts'));
+
+});
+
+gulp.task('removedist', function() { return del.sync('dist'); });
+gulp.task('clearcache', function () { return cache.clearAll(); });
+
+gulp.task('default', ['watch']);
