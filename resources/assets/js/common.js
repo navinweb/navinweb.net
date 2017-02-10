8a4590f60f0eafbc65519b20b8e0a18e0b7d152d
navinweb <anatoliykirzo@gmail.com>
2017-02-10 21:16:01 +0300

    #3 add form ajax handling + modal

--- resources/assets/js/common.js
+++ resources/assets/js/common.js
@@ -1,14 +1,46 @@
 $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 500);
-        return false;
-      }
-    }
-  });
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 500);
+				return false;
+			}
+		}
+	});
+});
+
+$(function() {
+	$('#contact-form').submit(function(e) { 
+		e.preventDefault() 
+		var btn = $('#contact-form .blu-btn');
+		var th = $(this);
+		var wrapMsg = $('.form_msg');
+
+		btn.attr('disabled','disabled');
+
+		$.ajax({
+			type: "POST",
+			url: "/main/form/sendEmail", //Change
+			data: th.serialize(),
+			success : function(data){
+				$('.modal>*').fadeIn();
+				console.log('нормич');
+				th.trigger("reset");
+				btn.prop("disabled", false);
+			},
+			error:function(data){
+				console.log('срань');
+			}
+		});
+
+		$('.modal-close').on('click', function(){
+			$('.modal>*').fadeOut();
+		})
+
+		return false;
+	});
 });
